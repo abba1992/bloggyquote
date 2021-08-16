8b9b00e53c86a7388e26a61b879a889615eb48e0
Abba Suraj <surajabba650@gmail.com>
2021-08-16 15:32:07 +0100

    ...

--- src/components/Intro/Para/Para.js
+++ src/components/Intro/Para/Para.js
@@ -4,7 +4,7 @@ const Para = () => {
     return (
         <div className={classes.Para}>
             <p>
-                Do your work. Not just your work, do a little more, but that little is worth more than all the rest. And if you suffer, as you must, and if you doubt, as you must, do your work. Put your heart into it, and the sky will clear. Then out of your doubt and suffering will be born the supreme joy of life.
+                "Do your work. Not just your work and no more, but a little more for the lavishing's sake. That little more which is worth all the rest. And if you doubt as you must; and if you suffer as you must-- do your work. Put your heart into it and the sky will clear. And then out of your very doubt and suffering will be born the supreme joy of life." - Dean Briggs
             </p>
         </div>
     )
